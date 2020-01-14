3ec2d3ab6c5d6a54d71a2b9fb77c1b737c842738
mrjleo <mrjleo@users.noreply.github.com>
2020-01-14 23:10:29 +0100

    add train_model_bce_batches

--- training.py
+++ training.py
@@ -1,10 +1,25 @@
 import os
 import csv
+import itertools
 
 import torch
 from tqdm import tqdm
 
 from qa_utils.misc import Logger
+
+
+def save_args(args_file, args):
+    """Save all arguments in a file.
+    
+    Arguments:
+        args_file {str} -- The .csv file to save
+        args {argparse.Namespace} -- Command line arguments
+    """
+    print('writing {}...'.format(args_file))
+    with open(args_file, 'w') as fp:
+        writer = csv.writer(fp)
+        for arg in vars(args):
+            writer.writerow([arg, getattr(args, arg)])
 
 
 def train_model_bce(model, train_dl, optimizer, args, device):
@@ -23,13 +38,8 @@ def train_model_bce(model, train_dl, optimizer, args, device):
     os.makedirs(ckpt_dir, exist_ok=True)
     logger = Logger(log_file, ['epoch', 'loss'])
 
-    # save all args in a file
     args_file = os.path.join(args.working_dir, 'args.csv')
-    print('writing {}...'.format(args_file))
-    with open(args_file, 'w') as fp:
-        writer = csv.writer(fp)
-        for arg in vars(args):
-            writer.writerow([arg, getattr(args, arg)])
+    save_args(args_file, args)
 
     criterion = torch.nn.BCEWithLogitsLoss()
     model.train()
@@ -46,7 +56,52 @@ def train_model_bce(model, train_dl, optimizer, args, device):
             loss_sum += loss.item()
 
         epoch_loss = loss_sum / len(train_dl)
-        print('epoch {} -- loss: {}'.format(epoch + 1, epoch_loss))
+        logger.log([epoch + 1, epoch_loss])
+
+        state = {'epoch': epoch + 1, 'state_dict': model.module.state_dict(),
+                 'optimizer': optimizer.state_dict()}
+        fname = os.path.join(ckpt_dir, 'weights_{:03d}.pt'.format(epoch + 1))
+        print('saving {}...'.format(fname))
+        torch.save(state, fname)
+
+
+def train_model_bce_batches(model, train_dl, optimizer, args, device):
+    """Train a model using binary cross entropy. Save the model after a number of batches and log
+    the loss in a file.
+
+    Arguments:
+        model {torch.nn.Module} -- The model to train
+        train_dl {torch.utils.data.DataLoader} -- Train dataloader
+        optimizer {torch.optim.Optimizer} -- Optimizer
+        args {argparse.Namespace} -- All command line arguments
+        device {torch.device} -- Device to train on
+    """
+    ckpt_dir = os.path.join(args.working_dir, 'ckpt')
+    log_file = os.path.join(args.working_dir, 'train.csv')
+    os.makedirs(ckpt_dir, exist_ok=True)
+    logger = Logger(log_file, ['epoch', 'loss'])
+
+    args_file = os.path.join(args.working_dir, 'args.csv')
+    save_args(args_file, args)
+
+    criterion = torch.nn.BCEWithLogitsLoss()
+    train_inf = itertools.cycle(train_dl)
+
+    model.train()
+    for epoch in range(args.epochs):
+        loss_sum = 0
+        optimizer.zero_grad()
+        for i in tqdm(range(args.save_after)):
+            b_x, b_y = next(train_inf)
+            out = model(b_x.to(device))
+            loss = criterion(out, b_y.to(device)) / args.accumulate_batches
+            loss.backward()
+            if (i + 1) % args.accumulate_batches == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+            loss_sum += loss.item()
+
+        epoch_loss = loss_sum / args.save_after
         logger.log([epoch + 1, epoch_loss])
 
         state = {'epoch': epoch + 1, 'state_dict': model.module.state_dict(),
@@ -72,13 +127,8 @@ def train_model_multi_bce(model, train_dl, optimizer, args, device):
     os.makedirs(ckpt_dir, exist_ok=True)
     logger = Logger(log_file, ['epoch', 'loss'])
 
-    # save all args in a file
     args_file = os.path.join(args.working_dir, 'args.csv')
-    print('writing {}...'.format(args_file))
-    with open(args_file, 'w') as fp:
-        writer = csv.writer(fp)
-        for arg in vars(args):
-            writer.writerow([arg, getattr(args, arg)])
+    save_args(args_file, args)
 
     # load BERT weights
     if args.bert_weights is not None:
