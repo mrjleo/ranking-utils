e5b94d558f297ae70f3e13835339ebc8c7d3e456
mrjleo <mrjleo@users.noreply.github.com>
2020-11-10 08:37:20 -0500

    use the 2019 TREC DL testset for MS MARCO

--- datasets/msmarco.py
+++ datasets/msmarco.py
@@ -1,5 +1,4 @@
 import csv
-import pickle
 import argparse
 from pathlib import Path
 from collections import defaultdict
@@ -10,7 +9,7 @@ from qa_utils.datasets.dataset import Dataset
 
 
 class MSMARCO(Dataset):
-    """MS MARCO passage ranking dataset class.
+    """MS MARCO passage ranking dataset class. Uses the testset of the 2019 TREC DL track.
 
     Args:
         args (argparse.Namespace): Namespace that contains the arguments defined below
@@ -19,11 +18,12 @@ class MSMARCO(Dataset):
     """
     def __init__(self, args: argparse.Namespace, num_negatives: int, query_limit: int):
         base_dir = Path(args.MSMARCO_DIR)
-        split_file = Path(args.SPLIT_FILE)
 
-        # read all queries
+        # read queries
         queries = {}
-        for f_name, num_lines in zip(['queries.train.tsv', 'queries.dev.tsv'], [808731, 101093]):
+        for f_name, num_lines in [('queries.train.tsv', 808731),
+                                  ('queries.dev.tsv', 101093),
+                                  ('msmarco-test2019-queries.tsv', 200)]:
             f = base_dir / f_name
             print(f'reading {f}...')
             with open(f, encoding='utf-8') as fp:
@@ -33,28 +33,39 @@ class MSMARCO(Dataset):
 
         # read documents
         docs = {}
-        docs_file = base_dir / 'collection.tsv'
-        print(f'reading {docs_file}...')
-        with open(docs_file, encoding='utf-8') as fp:
+        f = base_dir / 'collection.tsv'
+        print(f'reading {f}...')
+        with open(f, encoding='utf-8') as fp:
             reader = csv.reader(fp, delimiter='\t')
             for doc_id, doc in tqdm(reader, total=8841823):
                 docs[doc_id] = doc
 
-        # read all qrels
+        # read qrels
         qrels = defaultdict(dict)
         q_ids = defaultdict(set)
-        for f_name, num_lines in zip(['qrels.train.tsv', 'qrels.dev.tsv'], [532761, 59273]):
+        for f_name, num_lines in [('qrels.train.tsv', 532761), ('qrels.dev.tsv', 59273)]:
             f = base_dir / f_name
             print(f'reading {f}...')
             with open(f, encoding='utf-8') as fp:
                 reader = csv.reader(fp, delimiter='\t')
-                for q_id, _, doc_id, _ in tqdm(reader, total=num_lines):
-                    qrels[q_id][doc_id] = 1
+                for q_id, _, doc_id, rel in tqdm(reader, total=num_lines):
+                    qrels[q_id][doc_id] = int(rel)
                     q_ids[f_name].add(q_id)
 
-        # read all top documents
+        # TREC qrels have a different format
+        f = base_dir / '2019qrels-pass.txt'
+        print(f'reading {f}...')
+        with open(f, encoding='utf-8') as fp:
+            for q_id, _, doc_id, rel in csv.reader(fp, delimiter=' '):
+                # 1 is considered irrelevant
+                qrels[q_id][doc_id] = int(rel) - 1
+                q_ids['2019qrels-pass.txt'].add(q_id)
+
+        # read top documents
         pools = defaultdict(set)
-        for f_name, num_lines in zip(['top1000.train.txt', 'top1000.dev.tsv'], [478016942, 6668967]):
+        for f_name, num_lines in [('top1000.train.txt', 478016942),
+                                  ('top1000.dev.tsv', 6668967),
+                                  ('msmarco-passagetest2019-top1000.tsv', 189877)]:
             f = base_dir / f_name
             print(f'reading {f}...')
             with open(f, encoding='utf-8') as fp:
@@ -62,12 +73,11 @@ class MSMARCO(Dataset):
                 for q_id, doc_id, _, _ in tqdm(reader, total=num_lines):
                     pools[q_id].add(doc_id)
 
-        print(f'reading {split_file}...')
-        with open(split_file, 'rb') as fp:
-            val_ids = pickle.load(fp)
-
-        train_ids = q_ids['qrels.train.tsv']
-        test_ids = q_ids['qrels.dev.tsv'] - val_ids
+        # some IDs have no pool or no query -- remove them
+        all_ids = set(pools.keys()) & set(queries.keys())
+        train_ids = q_ids['qrels.train.tsv'] & all_ids
+        val_ids = q_ids['qrels.dev.tsv'] & all_ids
+        test_ids = q_ids['2019qrels-pass.txt'] & all_ids
         super().__init__(queries, docs, qrels, pools, train_ids, val_ids, test_ids, num_negatives, query_limit)
 
     @staticmethod
@@ -80,4 +90,3 @@ class MSMARCO(Dataset):
         """
         sp = subparsers.add_parser(name)
         sp.add_argument('MSMARCO_DIR', help='MS MARCO passage ranking dataset directory')
-        sp.add_argument('SPLIT_FILE', help='MS MARCO split')
