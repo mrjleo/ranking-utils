d673a558f9d2cc14c06a07670c7db0027956695d
mrjleo <mrjleo@users.noreply.github.com>
2022-04-01 17:02:21 +0200

    add type hint

--- src/ranking_utils/scripts/create_h5_data.py
+++ src/ranking_utils/scripts/create_h5_data.py
@@ -10,7 +10,7 @@ from pytorch_lightning import seed_everything
 
 
 @hydra.main(config_path="config", config_name="create_h5_data")
-def main(config: DictConfig):
+def main(config: DictConfig) -> None:
     seed_everything(config.random_seed)
     instantiate(config.dataset).save(
         Path.cwd(), config.num_training_negatives, config.balance_training_pairs
